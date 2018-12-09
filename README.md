00ae330eef7220f93d6087f8cded59c205c4db9d
ibaveda <igorbaveda@gmail.com>
2018-12-08 20:11:25 -0700

    changes,typos

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # GifTastic
 
 ### This cool page displays gifs from giphy.
-### You can press a button and it will show 10 gifs associated with the topic name shown on the button.
-### My choice was sports, the idea is/was to have an array of sports, those sports that I already selected would be displaying in the page. The user has the choice of adding different sports using the submit button provided.
+## You can press a button and it will show 10 gifs associated with the topic name shown on the button.
+## My choice was sports, the idea is/was to have an array of sports, those sports that I already selected would be displaying in the page. The user has the choice of adding different sports using the submit button provided.
+# As I have learned a little too late, using specific sports might not have been the best idea.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,91 +1,106 @@
-var sports = ["Basketball", "Baseball", "Football", "Hockey", "Soccer", "Skiing", "Climbing", "Ping-Pong", "Volleyball"];
-      
-    //Function to create(render) buttons
-    function renderButtons() {
-      $(".buttons-view").empty();
-      for (var i = 0; i < sports.length; i++) {
-        var gifButton = $("<button>");
-          gifButton.addClass("sport btn btn-default");
-          gifButton.attr("data-name", sports[i]);
-          gifButton.text(sports[i]);
-          $(".buttons-views").append(gifButton);
-          console.log(sports[i]);
-      }
-    };
-      
+var sports = [
+  "Basketball",
+  "Baseball",
+  "Football",
+  "Hockey",
+  "Soccer",
+  "Skiing",
+  "Climbing",
+  "Ping-Pong",
+  "Volleyball"
+];
 
-    $("#add-gif").on("click", function(event) {
-      event.preventDefault();
-      
-    
-      var gif = $("#gif-input").val().trim();
-      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=9QC38vpbZuKtfCNXGmvxTy1w5afiZrDm&q=" + gif +"gif&limit=10&offset=&rating=PG-13&lang=en";
+//Function to create(render) buttons
+function renderButtons() {
+  $(".buttons-view").empty();
+  for (var i = 0; i < sports.length; i++) {
+    var gifButton = $("<button>");
+    gifButton.addClass("sport btn btn-default");
+    gifButton.attr("data-name", sports[i]);
+    gifButton.text(sports[i]);
+    $(".buttons-view").append(gifButton);
+    console.log(sports[i]);
+  }
+}
 
+$("#add-gif").on("click", function(event) {
+  event.preventDefault();
 
-      //Create AJAX call for the gif
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
+  var gif = $("#gif-input")
+    .val()
+    .trim();
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?api_key=9QC38vpbZuKtfCNXGmvxTy1w5afiZrDm&q=" +
+    gif +
+    "gif&limit=10&offset=&rating=PG-13&lang=en";
 
-        if (response.data.length == 0) {
-            alert("No Gifs found for sport");
-        }
-        else if (sports.indexOf(gif) != -1) {
-            alert("This sport has been entered already");
-        }
-        else {
-        sports.push(gif);
-        renderButtons();
-            console.log(response);
-    }
-
-
-
-
-      });
-
+  //Create AJAX call for the gif
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      sports.push(gif);
+      renderButtons();
+      console.log(response);
     
   });
+});
 
-      
+//Function to display gifs
+function displayGifs() {
+  var gif = $(this).attr("data-name");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?api_key=9QC38vpbZuKtfCNXGmvxTy1w5afiZrDm&q=" +
+    gif +
+    "gif&limit=10&offset=&rating=PG-13&lang=en";
 
-      //Function to display gifs
-        function displayGifs() {
-            var gif = $(this).attr("data-name");
-            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=9QC38vpbZuKtfCNXGmvxTy1w5afiZrDm&q=" + gif +"gif&limit=10&offset=&rating=PG-13&lang=en";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
 
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response){
-console.log(response);
+    $(".gifs-view").empty();
+    for (var i = 0; i < response.data.length; i++) {
+      var gifDiv = $("<div>");
+      gifDiv.addClass("gifDiv");
+      gifDiv.html("<p>Rating: " + response.data[i].rating + "</p>");
 
-                $(".gifs-view").empty();
-                for (var i = 0; i < response.data.length; i++) {
-                    var gifDiv = $("<div>");
-                    gifDiv.addClass("gifDiv");
-                    gifDiv.html("<p>Rating" + response.data[i].rating + "</p>");
+      var gifImage = $(
+        "<img src='" + response.data[i].images.fixed_height_still.url + "'>"
+      );
+      gifImage.addClass("gif");
 
-                    var gifImage = $("<img src='" + response.data[i].images.fixed_height_still.url + "'>");
-                    gifImage.addClass("gif");
+      var imageDiv = $("<div>");
+      imageDiv.addClass("play");
+      imageDiv.attr("data-state", "still");
+      imageDiv.attr("data-name", gif);
+      imageDiv.attr(
+        "data-still",
+        response.data[i].images.fixed_height_still.url
+      );
+      imageDiv.attr("data-animate", response.data[i].images.fixed_height.url);
 
-                    var imageDiv = $("<div>");
-                    imageDiv.addClass("play");
-                    imageDiv.attr("data-state", "still");
-                    imageDiv.attr("data-name", gif);
-                    imageDiv.attr("data-still", reponse.data[i].images.fixed_height_still.url);
-                    imageDiv.attr("data-animate", response.data[i].images.fixed_height.url)
+      $(imageDiv).append(gifImage);
+      $(gifDiv).append(imageDiv);
+      $(".gifs-view").append(gifDiv);
+    }
+  });
+}
 
-                    $(imageDiv).append(gifImage);
-                }
-            });
-        }
-
-            //Function to play gifs from still
+//Function to play gifs from still
+        function playGif() {
+            if($(this).attr("data-state") == "still") {
+                $(this).html("<img src='" + $(this).attr("data-animate") + "'>");
+                $(this).attr("data-state", "animate");
+            }
+            else {
+                $(this).html("<img src='" + $(this).attr("data-still") + "'>");
+                $(this).attr("data-state", "still");
+            }
+        };
         
-        $(document).on("click", ".sport", displayGifs);
-        
 
-     renderButtons();   
-    
+$(document).on("click", ".sport", displayGifs);
+$(document).on("click", ".play", playGif);
+renderButtons();
